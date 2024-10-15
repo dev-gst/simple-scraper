0c0b879b2b27c0522ca36994481b5168c57fb657
Gustavo Garcia <gst.garc@gmail.com>
2024-10-15 02:45:52 -0300

    feat: create class to handle html parse operations

---
+++ src/main/java/scraper/service/ParserService.java
@@ -0,0 +1,74 @@
+package scraper.service;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class ParserService {
+
+    public static String parseLink(Document doc, String currentSubTask) {
+        System.out.println("Parsing HTML...");
+        String newUri = null;
+
+        Elements links = doc.getElementsByTag("a");
+        for (Element link : links) {
+            if (link.text().contains(currentSubTask)) {
+                newUri = link.attr("href");
+                break;
+            }
+        }
+
+        if (newUri == null) {
+            throw new NoSuchElementException("Could not find link for specified task: " + currentSubTask);
+        }
+
+        return newUri;
+    }
+
+    public static List<String> parseTissHistory(Document doc) {
+        Elements tableRows = doc.select("tr");
+        if (tableRows.isEmpty()) {
+            throw new RuntimeException("Could not find table rows");
+        }
+
+        Elements parsedRows = new Elements();
+        for (Element row : tableRows) {
+
+            Element parsedRow = new Element("tr");
+            for (int i = 0; i < 3; i++) {
+                Element td = row.child(0);
+
+                if (i == 2) {
+                    td.text(td.text().trim());
+                } else {
+                    td.text(td.text().trim() + ",");
+                }
+
+                parsedRow.appendChild(td);
+            }
+
+            parsedRows.add(parsedRow);
+
+            if (parsedRow.text().contains("Jan/2016")) {
+                break;
+            }
+        }
+
+        List<String> lines = new ArrayList<>();
+
+        String header = parsedRows.get(0).text().replaceAll(",\\s", ",");
+        lines.add(header);
+
+        for (int i = parsedRows.size() - 1; i >= 1; i--) {
+            String line = parsedRows.get(i).text().replaceAll(",\\s", ",");
+
+            lines.add(line);
+        }
+
+        return lines;
+    }
+}
