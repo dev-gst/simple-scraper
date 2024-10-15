82fa4b52fec75acbc54b2c396fa90dff8dc247dd
Gustavo Garcia <gst.garc@gmail.com>
2024-10-15 02:46:48 -0300

    feat: start the services in Main

--- src/main/java/scraper/Main.java
+++ src/main/java/scraper/Main.java
@@ -1,8 +1,11 @@
 package scraper;
 
+import scraper.service.ScraperService;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ScraperService scraperService = new ScraperService();
+        scraperService.start();
     }
 }
