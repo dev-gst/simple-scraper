ff64cd7a2c0e260a7e9c3af0ed71993b224a5dbf
Gustavo Garcia <gst.garc@gmail.com>
2024-10-16 02:29:36 -0300

    feat: implement necessary config files to be handled by the EmailService

---
+++ src/main/java/scraper/config/Env.java
@@ -0,0 +1,49 @@
+package scraper.config;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class Env {
+    private static final String FILE_PATH = "src/main/resources/config/application.yaml";
+
+    private static final String EMAIL_HOST;
+    private static final String EMAIL_PORT;
+    private static final String EMAIL_USERNAME;
+    private static final String EMAIL_PASSWORD;
+
+    static {
+        Yaml yaml = new Yaml();
+        Map<String, Map<String, String>> config;
+
+        try (InputStream applicationConfigs = Files.newInputStream(Paths.get(FILE_PATH))) {
+            config = yaml.load(applicationConfigs);
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading application configs", e);
+        }
+
+        EMAIL_HOST = config.get("email").get("host");
+        EMAIL_PORT = config.get("email").get("port");
+        EMAIL_USERNAME = config.get("email").get("username");
+        EMAIL_PASSWORD = config.get("email").get("password");
+    }
+
+    public static String getEmailHost() {
+        return EMAIL_HOST;
+    }
+
+    public static String getEmailPort() {
+        return EMAIL_PORT;
+    }
+
+    public static String getEmailUsername() {
+        return EMAIL_USERNAME;
+    }
+
+    public static String getEmailPassword() {
+        return EMAIL_PASSWORD;
+    }
+}
